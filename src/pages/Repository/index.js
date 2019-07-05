757192f25986e1915d983a16f8244219aa03bb93
andrevonah <andre2889@gmail.com>
2019-07-05 11:32:00 -0300

    passando o repositório como parâmetro para a sua rota

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function Respository() {
-  return <h1>Repo</h1>;
+export default function Respository({ match }) {
+  return <h1>Repo: {decodeURIComponent(match.params.repository)}</h1>;
 }
